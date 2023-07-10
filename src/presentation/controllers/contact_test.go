ffe01c93523fcd90fe2f8a213bef98c88ed4730c
Ricardo Conceição <ricardo220893@gmail.com>
2023-07-10 13:14:57 -0300

    test: ensure ContactController returns 400 if no email is provided

--- src/presentation/controllers/contact_test.go
+++ src/presentation/controllers/contact_test.go
@@ -34,3 +34,29 @@ func TestCreateContactBadRequest_NoNameProvided(t *testing.T) {
 		}
 	}
 }
+
+func TestCreateContactBadRequest_NoEmailProvided(t *testing.T) {
+	sut := NewContactController()
+	body, _ := json.Marshal(map[string]string{
+		"name":   "john.doe",
+		"phone":   "1234567890",
+		"address": "123 Main St",
+	})
+	r, _ := http.NewRequest("POST", "http://localhost:8080/contacts", bytes.NewBuffer(body))
+	r.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+
+	sut.handle(w, r)
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("Expected status code %v, got %v with body", http.StatusBadRequest, w.Code)
+	}
+	if w.Code == http.StatusBadRequest {
+		expected := map[string]string{"message": "email is required"}
+		var responseBody map[string]string
+		json.Unmarshal(w.Body.Bytes(), &responseBody)
+		if reflect.DeepEqual(expected, responseBody) != true {
+			t.Errorf("Expected response body %v, got %v", expected, responseBody)
+		}
+	}
+}
