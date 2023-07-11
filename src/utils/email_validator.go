85865964f412692ab2153c5fd8d68713a479da87
Ricardo Conceição <ricardo220893@gmail.com>
2023-07-11 17:51:12 -0300

    feat: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email_validator.go
+++ src/utils/email_validator.go
@@ -1,11 +1,23 @@
 package utils
 
-type EmailValidatorAdapter struct {}
+import "net/mail"
 
-func NewEmailValidatorAdapter() *EmailValidatorAdapter {
-	return &EmailValidatorAdapter{}
+type AddressParser interface {
+	Parse(email string) (*mail.Address, error)
+}
+
+type EmailValidatorAdapter struct {
+	AddressParser AddressParser
+}
+
+func NewEmailValidatorAdapter(addressParser AddressParser) *EmailValidatorAdapter {
+	return &EmailValidatorAdapter{addressParser}
 }
 
 func (v EmailValidatorAdapter) IsValid(email string) (bool, error) {
-	return false, nil
+	isValid, err := v.AddressParser.Parse(email)
+	if err != nil {
+		return false, err
+	}
+	return isValid != nil, nil
 }
