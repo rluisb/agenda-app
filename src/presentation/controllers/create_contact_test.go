ca6d2ee5d084ed2a45ce8850ca9bd6869773406e
Ricardo Conceição <ricardo220893@gmail.com>
2023-07-11 13:57:23 -0300

    test: ensure CreactContactController returns 400 if an invalid email is provided

--- src/presentation/controllers/create_contact_test.go
+++ src/presentation/controllers/create_contact_test.go
@@ -9,40 +9,58 @@ import (
 	"testing"
 )
 
+var IsValid func (email string) bool
+
+type EmailValidatorStub struct {
+}
+
+func NewEmailValidator() EmailValidatorStub {
+	return EmailValidatorStub{}
+}
+
+func (e EmailValidatorStub) IsValid(email string) bool { 
+	return IsValid(email)
+}
+
 func makeSut() *CreateContactController {
-	return NewCreateContactController()
+	emailValidatorStub := NewEmailValidator()
+	return NewCreateContactController(emailValidatorStub)
+}
+
+type Contact struct {
+	Name    string
+	Email   string
+	Phone   string
+	Address string
 }
 
 func TestCreateContactBadRequest_MissingRequiredField(t *testing.T) {
-	sut := makeSut()
-	type Contact struct {
-		Name         string
-		Email        string
-		Phone        string
-		Address      string
+	IsValid = func (email string) bool {
+		return true
 	}
 
+	sut := makeSut()
 
 	table := map[string]Contact{
 		"name": {
-			Email:        "john.doe@mail.com",
-			Phone:        "1234567890",
-			Address:      "123 Main St",
+			Email:   "john.doe@mail.com",
+			Phone:   "1234567890",
+			Address: "123 Main St",
 		},
 		"email": {
-			Name:         "john.doe",
-			Phone:        "1234567890",
-			Address:      "123 Main St",
+			Name:    "john.doe",
+			Phone:   "1234567890",
+			Address: "123 Main St",
 		},
 		"phone": {
-			Name:         "john.doe",
-			Email:        "john.doe@mail.com",
-			Address:      "123 Main St",
+			Name:    "john.doe",
+			Email:   "john.doe@mail.com",
+			Address: "123 Main St",
 		},
 		"address": {
-			Name:         "john.doe",
-			Email:        "john.doe@mail.com",
-			Phone:        "1234567890",
+			Name:  "john.doe",
+			Email: "john.doe@mail.com",
+			Phone: "1234567890",
 		},
 	}
 
@@ -63,6 +81,36 @@ func TestCreateContactBadRequest_MissingRequiredField(t *testing.T) {
 			if reflect.DeepEqual(expected, responseBody) != true {
 				t.Errorf("Expected response body %v, got %v", expected, responseBody)
 			}
+		}
+	}
+}
+
+func TestCreateContactBadRequest_InvalidEmailProvided(t *testing.T) {
+	IsValid = func (email string) bool {
+		return false
+	}
+	sut := makeSut()
+	contatc := Contact{
+		Name:    "John Doe",
+		Email:   "invalid_email@mail.com",
+		Phone:   "1234567890",
+		Address: "123 Main St",
+	}
+	body, _ := json.Marshal(contatc)
+	r, _ := http.NewRequest("POST", "http://localhost:8080/contacts", bytes.NewBuffer(body))
+	r.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	sut.handle(w, r)
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("Expected status code %v, got %v with body", http.StatusBadRequest, w.Code)
+	}
+	if w.Code == http.StatusBadRequest {
+		expected := map[string]string{"message": "invalid email"}
+		var responseBody map[string]string
+		json.Unmarshal(w.Body.Bytes(), &responseBody)
+		if reflect.DeepEqual(expected, responseBody) != true {
+			t.Errorf("Expected response body %v, got %v", expected, responseBody)
 		}
 	}
 }
