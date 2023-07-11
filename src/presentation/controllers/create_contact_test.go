b16bdf2023c42fa3b046b6012fa5143a7d16c583
Ricardo Conceição <ricardo220893@gmail.com>
2023-07-11 16:32:22 -0300

    test: ensure CreateContactController calls AddContact with correct values

--- src/presentation/controllers/create_contact_test.go
+++ src/presentation/controllers/create_contact_test.go
@@ -8,22 +8,20 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
+
+	"github.com/rluisb/agenda-app/src/domain/usecases"
+	"github.com/rluisb/agenda-app/src/domain/usecases/models"
 )
 
 var IsValid func(email string) (bool, error)
 
-type EmailValidatorSpy struct {
-	IsValidCallCount  int
-	IsValidCalledWith string
+type GenericSpy struct {
+	CallCount  int
+	CalledWith interface{}
 }
 
-func NewEmailValidatorSpy() *EmailValidatorSpy {
-	return &EmailValidatorSpy{}
-}
-
-func (spy *EmailValidatorSpy) reset() {
-	spy.IsValidCallCount = 0
-	spy.IsValidCalledWith = ""
+func NewGenericSpy() *GenericSpy {
+	return &GenericSpy{}
 }
 
 type EmailValidatorStub struct {
@@ -38,15 +36,9 @@ func (e EmailValidatorStub) IsValid(email string) (bool, error) {
 }
 
 func makeSut() *CreateContactController {
+	addContactStub := NewAddContactStub()
 	emailValidatorStub := NewEmailValidator()
-	return NewCreateContactController(emailValidatorStub)
-}
-
-type Contact struct {
-	Name    string
-	Email   string
-	Phone   string
-	Address string
+	return NewCreateContactController(emailValidatorStub, addContactStub)
 }
 
 func TestCreateContactBadRequest_MissingRequiredField(t *testing.T) {
@@ -55,27 +47,12 @@ func TestCreateContactBadRequest_MissingRequiredField(t *testing.T) {
 	}
 	sut := makeSut()
 
-	table := map[string]Contact{
-		"name": {
-			Email:   "john.doe@mail.com",
-			Phone:   "1234567890",
-			Address: "123 Main St",
-		},
-		"email": {
-			Name:    "john.doe",
-			Phone:   "1234567890",
-			Address: "123 Main St",
-		},
-		"phone": {
-			Name:    "john.doe",
-			Email:   "john.doe@mail.com",
-			Address: "123 Main St",
-		},
-		"address": {
-			Name:  "john.doe",
-			Email: "john.doe@mail.com",
-			Phone: "1234567890",
-		},
+	
+	table := map[string]usecases.AddContactModel{
+		"name": *usecases.NewAddContactModel("", "john.doe@mail.com", "1234567890", "123 Main St"),
+		"email":  *usecases.NewAddContactModel("john.doe", "", "1234567890", "123 Main St"),
+		"phone": *usecases.NewAddContactModel("john.doe","john.doe@mail.com","","123 Main St"),
+		"address": *usecases.NewAddContactModel("john.doe", "john.doe@mail.com", "1234567890", ""),
 	}
 
 	for missingField, contact := range table {
@@ -100,19 +77,14 @@ func TestCreateContactBadRequest_MissingRequiredField(t *testing.T) {
 }
 
 func TestCreateContactBadRequest_InvalidEmailProvided(t *testing.T) {
-	emailValidatorSpy := NewEmailValidatorSpy()
+	emailValidatorSpy := NewGenericSpy()
 	IsValid = func(email string) (bool, error) {
-		emailValidatorSpy.IsValidCallCount++
-		emailValidatorSpy.IsValidCalledWith = email
+		emailValidatorSpy.CallCount++
+		emailValidatorSpy.CalledWith = email
 		return false, nil
 	}
 	sut := makeSut()
-	contact := Contact{
-		Name:    "John Doe",
-		Email:   "invalid_email@mail.com",
-		Phone:   "1234567890",
-		Address: "123 Main St",
-	}
+	contact := usecases.NewAddContactModel("John Doe", "invalid_email@mail.com", "1234567890", "123 Main St")
 	body, _ := json.Marshal(contact)
 	r, _ := http.NewRequest("POST", "http://localhost:8080/contacts", bytes.NewBuffer(body))
 	r.Header.Set("Content-Type", "application/json")
@@ -129,62 +101,24 @@ func TestCreateContactBadRequest_InvalidEmailProvided(t *testing.T) {
 		if reflect.DeepEqual(expectedResponse, responseBody) != true {
 			t.Errorf("Expected response body %v, got %v", expectedResponse, responseBody)
 		}
-		if emailValidatorSpy.IsValidCallCount != 1 {
-			t.Errorf("Expected email validator to be called 1 time, got %v", emailValidatorSpy.IsValidCallCount)
+		if emailValidatorSpy.CallCount != 1 {
+			t.Errorf("Expected email validator to be called 1 time, got %v", emailValidatorSpy.CallCount)
 		}
-		if emailValidatorSpy.IsValidCalledWith != contact.Email {
-			t.Errorf("Expected email validator to be called with %v, got %v", contact.Email, emailValidatorSpy.IsValidCalledWith)
-		}
-	}
-}
-
-func TestCreateContactWithSuccess_CorrectEmail(t *testing.T) {
-	emailValidatorSpy := NewEmailValidatorSpy()
-	IsValid = func(email string) (bool, error) {
-		emailValidatorSpy.IsValidCallCount++
-		emailValidatorSpy.IsValidCalledWith = email
-		return true, nil
-	}
-	sut := makeSut()
-	contact := Contact{
-		Name:    "John Doe",
-		Email:   "john.doe@mail.com",
-		Phone:   "1234567890",
-		Address: "123 Main St",
-	}
-	body, _ := json.Marshal(contact)
-	r, _ := http.NewRequest("POST", "http://localhost:8080/contacts", bytes.NewBuffer(body))
-	r.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-
-	sut.handle(w, r)
-	if w.Code != http.StatusOK {
-		t.Errorf("Expected status code %v, got %v with body", http.StatusOK, w.Code)
-	}
-	if w.Code == http.StatusOK {
-		expectedResponse := map[string]string{}
-		var responseBody map[string]string
-		json.Unmarshal(w.Body.Bytes(), &responseBody)
-		if len(responseBody) != len(expectedResponse) {
-			t.Errorf("Expected response body %v, got %v", expectedResponse, responseBody)
+		if emailValidatorSpy.CalledWith != contact.Email {
+			t.Errorf("Expected email validator to have been called with %v, got %v", contact.Email, emailValidatorSpy.CalledWith)
 		}
 	}
 }
 
 func TestCreateContactInternalServerError_IfEmailValidatorThrows(t *testing.T) {
-	emailValidatorSpy := NewEmailValidatorSpy()
+	emailValidatorSpy := NewGenericSpy()
 	IsValid = func(email string) (bool, error) {
-		emailValidatorSpy.IsValidCallCount++
-		emailValidatorSpy.IsValidCalledWith = email
+		emailValidatorSpy.CallCount++
+		emailValidatorSpy.CalledWith = email
 		return false, errors.New("something went wrong")
 	}
 	sut := makeSut()
-	contact := Contact{
-		Name:    "John Doe",
-		Email:   "invalid_email@mail.com",
-		Phone:   "1234567890",
-		Address: "123 Main St",
-	}
+	contact := usecases.NewAddContactModel("John Doe", "invalid_email@mail.com", "1234567890", "123 Main St")
 	body, _ := json.Marshal(contact)
 	r, _ := http.NewRequest("POST", "http://localhost:8080/contacts", bytes.NewBuffer(body))
 	r.Header.Set("Content-Type", "application/json")
@@ -201,11 +135,76 @@ func TestCreateContactInternalServerError_IfEmailValidatorThrows(t *testing.T) {
 		if reflect.DeepEqual(expectedResponse, responseBody) != true {
 			t.Errorf("Expected response body %v, got %v", expectedResponse, responseBody)
 		}
-		if emailValidatorSpy.IsValidCallCount != 1 {
-			t.Errorf("Expected email validator to be called 1 time, got %v", emailValidatorSpy.IsValidCallCount)
+		if emailValidatorSpy.CallCount != 1 {
+			t.Errorf("Expected email validator to be called 1 time, got %v", emailValidatorSpy.CallCount)
 		}
-		if emailValidatorSpy.IsValidCalledWith != contact.Email {
-			t.Errorf("Expected email validator to be called with %v, got %v", contact.Email, emailValidatorSpy.IsValidCalledWith)
+		if emailValidatorSpy.CalledWith != contact.Email {
+			t.Errorf("Expected email validator to have been called with %v, got %v", contact.Email, emailValidatorSpy.CalledWith)
+		}
+	}
+}
+
+var add func(contact *usecases.AddContactModel) *models.ContactModel
+
+type AddContactStub struct {
+}
+
+func NewAddContactStub() AddContactStub {
+	return AddContactStub{}
+}
+
+func (a AddContactStub) Add(contact *usecases.AddContactModel) *models.ContactModel {
+	return add(contact)
+}
+
+func TestCreateContactWithSuccess(t *testing.T) {
+	addAccountSpy := NewGenericSpy()
+	add = func(contact *usecases.AddContactModel) *models.ContactModel {
+		addAccountSpy.CallCount++
+		addAccountSpy.CalledWith = contact
+		return &models.ContactModel{
+			ID:      "1",
+			Name:    contact.Name,
+			Email:   contact.Email,
+			Phone:   contact.Phone,
+			Address: contact.Address,
+		}
+	}
+	emailValidatorSpy := NewGenericSpy()
+	IsValid = func(email string) (bool, error) {
+		emailValidatorSpy.CallCount++
+		emailValidatorSpy.CalledWith = email
+		return true, nil
+	}
+	sut := makeSut()
+	contact := usecases.NewAddContactModel("John Doe", "john.doe@mail.com", "1234567890", "123 Main St")
+	body, _ := json.Marshal(contact)
+	r, _ := http.NewRequest("POST", "http://localhost:8080/contacts", bytes.NewBuffer(body))
+	r.Header.Set("Content-Type", "application/json")
+	w := httptest.NewRecorder()
+
+	sut.handle(w, r)
+	if w.Code != http.StatusOK {
+		t.Errorf("Expected status code %v, got %v with body", http.StatusOK, w.Code)
+	}
+	if w.Code == http.StatusOK {
+		var responseBody models.ContactModel
+		json.Unmarshal(w.Body.Bytes(), &responseBody)
+		// if reflect.DeepEqual(contact, responseBody) != true {
+		// 	t.Errorf("Expected response body %v, got %v", contact, responseBody)
+		// }
+
+		if emailValidatorSpy.CallCount != 1 {
+			t.Errorf("Expected email validator to have been called 1 time, got %v", emailValidatorSpy.CallCount)
+		}
+		if reflect.DeepEqual(contact.Email, emailValidatorSpy.CalledWith) != true {
+			t.Errorf("Expected email validator to have been called with %v, got %v", contact.Email, emailValidatorSpy.CalledWith)
+		}
+		if addAccountSpy.CallCount != 1 {
+			t.Errorf("Expected add account to have been called 1 time, got %v", addAccountSpy.CallCount)
+		}
+		if reflect.DeepEqual(contact, addAccountSpy.CalledWith) != true {
+			t.Errorf("Expected add account to been called with %v, got %v", contact, addAccountSpy.CalledWith)
 		}
 	}
 }
