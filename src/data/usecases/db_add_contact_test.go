40fe71b8bea4e47b672d6172286453c90f5b1ad1
Ricardo Conceição <ricardo220893@gmail.com>
2023-07-12 16:52:05 -0300

    test: ensure DbAddAContact calls AddContactRepository with correct values

---
+++ src/data/usecases/db_add_contact_test.go
@@ -0,0 +1,51 @@
+package usecases
+
+import (
+	"testing"
+
+	"github.com/rluisb/agenda-app/src/domain/usecases"
+	"github.com/rluisb/agenda-app/src/domain/usecases/models"
+)
+
+type GenericSpy struct {
+	CallCount  int
+	CalledWith interface{}
+}
+
+func NewGenericSpy() *GenericSpy {
+	return &GenericSpy{}
+}
+
+var Add func(addContactModel *usecases.AddContactModel) (*models.ContactModel, error)
+
+type AddContactRepositoryStub struct{}
+
+func NewAddContactRepositoryStub() *AddContactRepositoryStub {
+	return &AddContactRepositoryStub{}
+}
+
+func (a AddContactRepositoryStub) Add(addContactModel *usecases.AddContactModel) (*models.ContactModel, error) {
+	return Add(addContactModel)
+}
+
+func TestDbAddContact(t *testing.T) {
+	t.Run("Should call AddContactRepository with correct values", func(t *testing.T) {
+		addContactRepositorySpy := NewGenericSpy()
+		Add = func(addContactModel *usecases.AddContactModel) (*models.ContactModel, error) {
+			addContactRepositorySpy.CallCount++
+			addContactRepositorySpy.CalledWith = addContactModel
+			contactModel := models.NewContactModel("1", "John Doe", "john.doe@mail.com", "1234567890", "123 Main St")
+			return contactModel, nil
+		}
+		addContactRepositoryStub := NewAddContactRepositoryStub()
+		sut := NewDbAddContact(addContactRepositoryStub)
+		addContactModel := usecases.NewAddContactModel("John Doe", "john.doe@mail.com", "1234567890", "123 Main St")
+		sut.Add(addContactModel)
+		if addContactRepositorySpy.CallCount != 1 {
+			t.Errorf("Expected call count to be 1, but got %d", addContactRepositorySpy.CallCount)
+		}
+		if addContactRepositorySpy.CalledWith != addContactModel {
+			t.Errorf("Expected called with to be %v, but got %v", addContactModel, addContactRepositorySpy.CalledWith)
+		}
+	})
+}
